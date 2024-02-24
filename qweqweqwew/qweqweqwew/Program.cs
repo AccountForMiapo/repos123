5990714519136fde37a447ff9138a8cbf37a159f
AccountForMiapo <161135493+AccountForMiapo@users.noreply.github.com>
2024-02-24 20:37:09 +0700

    myProject
    проект реализует красивую анимацию загрузки в командной строке.

---
+++ qweqweqwew/qweqweqwew/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System;
+while (true)
+{
+    
+    Console.Write("|");
+    Thread.Sleep(200);
+    Console.Clear();
+    Console.Write("/");
+    Thread.Sleep(200);
+    Console.Clear();
+    Console.Write("-");
+    Thread.Sleep(200);
+    Console.Clear();
+    Console.Write("\\");
+    Thread.Sleep(200);
+    Console.Clear();
+}
